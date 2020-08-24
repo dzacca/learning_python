c1c969c074c5353a576b9170cd7c561b65a99d74
Diego Zaccariotto <diego.zaccariotto@outlook.com>
2020-08-24 22:14:04 +0200

    Exercises 11 to 15

---
+++ lpthw/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+heigth = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you are {age} old, {heigth} tall and {weight} heavy.")
